68f5d88bfb25ec3de19b12b31a6acf3b6087a621
twishajain <twishajain@users.noreply.github.com>
2018-10-26 13:05:37 +0530

    Improved Conversation Flow

---
+++ ConversationFlowWithDialogs/Dialogs/Dialogs/RootDialog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Dialogs.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public string name;
+        public string age;
+        public string color;
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            PromptDialog.Text(context, this.NameReceived, "Hi! I am Firebot. Ask me something.");
+        }
+        private async Task NameReceived(IDialogContext context, IAwaitable<string> result)
+        {
+            name = await result;
+            PromptDialog.Text(context, this.AgeReceived, " Please Enter your area.");
+
+        }
+        private async Task AgeReceived(IDialogContext context, IAwaitable<string> result)
+        {
+            age = await result;
+            PromptDialog.Text(context, this.ColorReceived, "Thank you. I am scouring through images of"+age+" to find fires....  I found 1 fire in the area of California. These are its GPS coordinates. 37.7749° N, 122.4194° W");
+
+        }
+        private async Task ColorReceived(IDialogContext context, IAwaitable<string>result)
+        {
+            context.Done<object>(null);
+            
+            
+
+        }
+
+    }
+}
---
+++ ConversationFlowWithDialogs/README.md
@@ -0,0 +1,2 @@
+# ConversationFlowWithDialogs
+A simple bot built on Microsoft Bot Framework that demonstrates how to create a conversation flow with a user using dialogs and respond appropriately using a .NET file. This bot will ask a user their name, age and favorite color. 
